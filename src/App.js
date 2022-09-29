0adc8c05d36e34df3eca65e35a8b4709d7a34640
caseyvoss95 <casey.vos@bu.edu>
2022-09-29 16:13:41 -0400

    base pages created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { Route, Switch, Link } from "react-router-dom";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav></Nav>
+      <Switch>
+        <Route exact path="/">
+          <h1>login screen</h1>
+          <Link to="/order"><button>Login</button></Link>
+        </Route>
+        <Route path="/order">
+        <Main/>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+const Nav = () => {
+  return (
+    <nav>
+        Nav Bar
+    </nav>
+  )
+}
+
+export default Nav;
