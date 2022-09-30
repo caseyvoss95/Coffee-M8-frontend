ceb99a989b69403a4913d2ec6ef495f6c901d877
caseyvoss95 <casey.vos@bu.edu>
2022-09-30 09:49:25 -0400

    basic form built

--- src/components/DrinkForm.js
+++ src/components/DrinkForm.js
@@ -9,6 +9,22 @@ const DrinkForm = () => {
                 <option value="tea">tea</option>
                 <option value="earl grey">earl grey</option>
             </select>
+            <select>
+                <option value="small">small</option>
+                <option value="meduim">medium</option>
+                <option value="large">large</option>
+            </select>
+            <select>
+                <option value="hald and half">half and half</option>
+                <option value="whole milk">whole milk</option>
+                <option value="almond milk">almond milk</option>
+                <option value="oat milk">oat milk</option>
+            </select>
+            <select>
+                <option value="none">(none)</option>
+                <option value="pumpkin spice">pumpkin spice</option>
+                <option value="apple cinnamon">apple cinnamon</option>
+            </select>
         </form>
     )
 }
