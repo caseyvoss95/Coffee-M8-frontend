901aad3fad4080029c33f18e8fd69c285e4de850
caseyvoss95 <casey.vos@bu.edu>
2022-09-30 16:25:04 -0400

    refactored routes into Main

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Route, Switch, Link } from "react-router-dom";
 import Nav from "./components/Nav";
 import Main from "./components/Main";
 
@@ -8,15 +7,7 @@ function App() {
   return (
     <div className="App">
       <Nav></Nav>
-      <Switch>
-        <Route exact path="/">
-          <h1>login screen</h1>
-          <Link to="/order"><button>Login</button></Link>
-        </Route>
-        <Route path="/order">
         <Main/>
-        </Route>
-      </Switch>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Route, Switch, Link } from "react-router-dom";
 import DrinkForm from "./DrinkForm";
 import drinkSeed from "./drinkSeed";
 
@@ -11,14 +12,27 @@ const Main = () => {
     milk: drinkSeed.milk[0],
     flavor: drinkSeed.flavor[0],
     topping: drinkSeed.topping[0]
-});
+  });
 
-  
+
   return (
-    <div>
-        <h1>How would you like your drink?</h1>
-        <DrinkForm drink={drink} setDrink={setDrink}/>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <h1>login screen</h1>
+        <Link to="/order"><button>Login</button></Link>
+      </Route>
+      <Route path="/order">
+        <div>
+          <h1>How would you like your drink?</h1>
+          <DrinkForm drink={drink} setDrink={setDrink} />
+        </div>
+      </Route>
+    </Switch>
+
+
+
+
+
   )
 }
 
