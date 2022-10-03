c50c8c83a552ccdac385c03018a225fe49dc85f5
caseyvoss95 <casey.vos@bu.edu>
2022-10-03 11:38:22 -0400

    get drink list to host view successful

--- src/pages/HostView.js
+++ src/pages/HostView.js
@@ -1,13 +1,24 @@
-
+import { useState, useEffect } from "react";
 
 const HostView = () => {
   
+    const [drinkList, setDrinkList] = useState(null);
+
+    const URL = "http://localhost:4000/orders/allOrders"; //where orders live
+    const orderID = "63372013187b01b84368ce48"; //Dunkin order ID
   
-  
-  
-  
+    const getDrinks = async () => {
+       console.log('get Drinks is on')
+        const response = await fetch(URL);
+        const data = await response.json();
+        setDrinkList(data);
+    }
+
+    useEffect(() => {getDrinks()}, []);
+    console.log("drinkList after useEffect", drinkList);
+
     return (
-    <div>Host View Hello</div>
+    <div>Host View</div>
   )
 }
 
