ae154ef75595f0c5639cc267d51ac6a9c311e79f
adicerbo <dicerboantonio32@gmail.com>
2022-10-04 16:23:01 -0400

    add logout functionality to nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+import { auth } from "../firebase-config";
+import { signOut } from "firebase/auth";
+
+
 const Nav = () => {
+  const logout = async () => {
+    await signOut(auth);
+  }
   return (
     <nav className="navbar">
-        Nav Bar
+      Coffee-M8
+      <Link to="/"><button className="btn btn-secondary"  >Home</button></Link>
+      <button className="btn btn-danger btn-sm" id="logout" onClick={logout}>logout</button>
     </nav>
   )
 }
