ba238a8fe8a38af9d45bb03a48dac3e5c4d5c4cd
caseyvoss95 <casey.vos@bu.edu>
2022-10-05 14:49:40 -0400

    moved logged in user to nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,31 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../firebase-config";
 import { signOut } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
 
 
-const Nav = () => {
+const Nav = ({loggedIn, setLoggednIn}) => {
+  
+  const [user, setUser] = useState({})
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+});
+  
+  
+  
   const logout = async () => {
     await signOut(auth);
+    localStorage.clear();
   }
   return (
     <nav className="navbar">
       Coffee-M8
       <Link to="/"><button className="btn btn-secondary"  >Home</button></Link>
+      {user?.email}
       <button className="btn btn-danger btn-sm" id="logout" onClick={logout}>logout</button>
+      
     </nav>
   )
 }
