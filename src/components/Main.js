9dafc382b42b335141940cbd03f0d15fcffb0c7b
caseyvoss95 <casey.vos@bu.edu>
2022-10-06 13:53:06 -0400

    logged in as added

--- src/components/Main.js
+++ src/components/Main.js
@@ -62,6 +62,9 @@ const Main = () => {
   return (
     <div className="main">
       <Nav setLoggednIn={setLoggedIn} loggedIn={loggedIn} user={user} setUser={setUser} />
+      <div className="container" id="user">
+        logged in as: {user?.email}
+      </div>
       <Switch>
         <Route exact path="/">
           <Login setLoggedIn={setLoggedIn} />
